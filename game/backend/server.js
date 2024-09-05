61ae35196b644d05c963be067ea60c9f0ea73220
yuvalzim <126091309+yuvalzim@users.noreply.github.com>
2024-09-05 11:01:08 +0300

    Add files via upload

--- game/backend/server.js
+++ game/backend/server.js
@@ -1,20 +1,39 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
+
 
 const server = http.createServer( (req, res) => {
-    console.log(req);
+    console.log(req.url);
 
+    var currentPath = path.dirname(path.dirname(__filename));
 
     // Get the url
-    url = req.url.slice(1);
+    url = req.url;
     
     // Check for home page request
-    if (!url){
-        url = 'index.html'
+    if (url == '/'){
+        res.statusCode = 301;
+        res.setHeader('Location', '/login.html');
+        res.end();
+    }
+
+    if (url == "/login") {
+        url += ".html";
+    }
+
+    currentPath += url
+
+
+    if (fs.existsSync(currentPath)) {
+        res.statusCode = 200;
+    }
+    else {
+        res.statusCode = 404;
     }
 
     // Read the requested url and send it to the browser
-    fs.readFile('D:\\game\\' + url, (err, data) => {
+    fs.readFile(currentPath, (err, data) => {
         if (err){
             console.log(err);
         }
